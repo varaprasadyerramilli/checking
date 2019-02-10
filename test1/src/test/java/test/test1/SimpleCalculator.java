2ed72d5a49c9a6a87866d564fc08f3f16f8caefc
varaprasad <varaprasad@192.168.0.9>
2019-02-10 09:30:32 -0800

    commiting

---
+++ test1/src/test/java/test/test1/SimpleCalculator.java
@@ -0,0 +1,59 @@
+package test.test1;
+
+public class SimpleCalculator {
+	private double firstNumber,secondNumber;
+	
+	public double getFirstNumber()
+	{
+		return firstNumber;
+	}
+	public double getSecondNumber()
+	{
+		return secondNumber;
+	}
+	public void setFirstNumber(double firstNumber)
+	{
+		this.firstNumber=firstNumber;
+	}
+	public void setSecondNumber(double secondNumber)
+	{
+		this.secondNumber=secondNumber;
+	}
+	
+	public double getAdditionResult()
+	{
+		return firstNumber+secondNumber;
+	}
+	public double getSubtractionResult()
+	{
+		return firstNumber-secondNumber;
+	}
+	public double getMultiplicationResult()
+	{
+		return firstNumber*secondNumber;
+	}
+	public double getDivisionResult()
+	{
+		if(secondNumber==0)
+		{
+			return 0;
+		}
+		else
+		{
+			return firstNumber/secondNumber;
+		}
+	}
+	public static void main(String[] args)
+	{
+		SimpleCalculator calculator= new SimpleCalculator();
+		calculator.setFirstNumber(5.0);
+		calculator.setSecondNumber(4);
+		System.out.println("add= "+calculator.getAdditionResult());
+		System.out.println("add= "+calculator.getSubtractionResult());
+		calculator.setFirstNumber(5.25);
+		calculator.setSecondNumber(0);
+		System.out.println("multiply= "+calculator.getMultiplicationResult());
+		System.out.println("divide "+calculator.getDivisionResult());
+	}
+	 
+}
