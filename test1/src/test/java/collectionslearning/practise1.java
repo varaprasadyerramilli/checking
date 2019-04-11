23d9dabdc33a12bf2c2dfaca70a03acd822fe2c7
varaprasadyerramilli <varaprasadyerramilli@gmail.com>
2019-04-11 08:19:49 -0700

    fsd

---
+++ test1/src/test/java/collectionslearning/practise1.java
@@ -0,0 +1,14 @@
+package collectionslearning;
+
+import java.util.Collection;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+
+public class practise1 extends HashMap{
+
+	}
